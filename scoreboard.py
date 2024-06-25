7d47fa03738dc87813152ef4269ebf9979805f4a
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2024-06-25 15:37:03 +0000

    add in constants

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
 
 class Scoreboard(Turtle):
 
@@ -12,7 +14,7 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
