482c8fd422a2ddb12cb493295887025570ead8ea
muhammad9970 <muhammad.ahmad@crowncommercial.gov.uk>
2024-06-25 15:35:03 +0000

    Add in update scoreboard method to get rid of score bug

--- scoreboard.py
+++ scoreboard.py
@@ -8,10 +8,14 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
         self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
 
     def increase_score(self):
         self.score += 1
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.clear()
+        self.update_scoreboard()
 
